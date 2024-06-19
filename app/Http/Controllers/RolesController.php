a6df436817f0de9d7ee3ce3a7d161c5ebf68552c
Dom <leodomsolar@gmail.com>
2024-06-19 21:07:35 +0700

    Commit

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -5,18 +5,24 @@ namespace App\Http\Controllers;
 use App\Models\Roles;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 class RolesController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    protected $guard_name='web';
+     public function index()
     {
         $roles= Roles::all();
         return Inertia::render("Roles/Index",['roles'=>$roles]);
     }
 
+    public function getAll(){
+        $roles= Roles::all();
+        return response()->json(['check'=> true,'data'=> $roles]);
+    }
     /**
      * Show the form for creating a new resource.
      */
@@ -30,7 +36,19 @@ class RolesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:roles,name',
+          
+        ], [
+            'name.required' => 'Chưa nhận được loại tài khoản',
+            'name.unique' => 'Loại tài khoản bị trùng',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['check' => false, 'msg' => $validator->errors()->first()]);
+        }
+        Roles::create($request->all());
+        $roles= Roles::all();
+        return response()->json(['check'=> true,'data'=> $roles]);
     }
 
     /**
@@ -52,9 +70,22 @@ class RolesController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Roles $roles)
+    public function update(Request $request, Roles $roles,$id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'role' => 'unique:roles,name',
+          
+        ], [
+            'role.unique' => 'Loại tài khoản bị trùng',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['check' => false, 'msg' => $validator->errors()->first()]);
+        }
+        $data = $request->all();
+        Roles::where('id',$id)->update($data);
+        $roles= Roles::all();
+        return response()->json(['check'=> true,'data'=> $roles]);
+
     }
 
     /**
