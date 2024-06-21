dab9855b669ed3486dfc6b07299879028bec132e
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-06-21 11:25:38 +0700

    Commit

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
