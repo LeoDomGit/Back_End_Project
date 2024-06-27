ad6dbd2f732b1ff5e4cf0838566cd6c1f1eae09e
Dom <leodomsolar@gmail.com>
2024-06-27 19:32:32 +0700

    Commit

--- plugins/permissions/routes/web.php
+++ plugins/permissions/routes/web.php
@@ -4,4 +4,5 @@ use Leo\Permissions\Controllers\PermissionController;
 use App\Http\Middleware\CheckLogin;
 Route::middleware(['web', CheckLogin::class])->group(function () {
     Route::resource('permissions', PermissionController::class);
+
 });
