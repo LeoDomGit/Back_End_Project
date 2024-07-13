afbee282f2fe4af6b0458f20a277d4a2b4572dbb
Khanh Duy Dev <khanhduytran1803@gmail.com>
2024-07-13 16:15:49 +0700

    new commit

--- resources/js/Pages/Bookings/Index.jsx
+++ resources/js/Pages/Bookings/Index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
 import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
-import { Button, Modal } from "react-bootstrap";
+import { Row, Col, Form, Button, Modal } from "react-bootstrap";
 
 function Index({ bookings }) {
 	const [data, setData] = useState([]);
@@ -22,7 +22,6 @@ function Index({ bookings }) {
 			headerName: "Người đặt",
 			width: 150,
 			editable: true,
-			valueGetter: (params) => (params.row.user ? params.row.user.name : ""),
 		},
 		{
 			field: "id_customer",
@@ -44,39 +43,79 @@ function Index({ bookings }) {
 		},
 		{
 			field: "end_time",
-			headerName: "Thời hạn",
+			headerName: "Thời hạn chời lịch",
 			width: 200,
 			editable: true,
 		},
-		// {
-		// 	field: "fullName",
-		// 	headerName: "Full name",
-		// 	description: "This column has a value getter and is not sortable.",
-		// 	sortable: false,
-		// 	width: 160,
-		// 	valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
-		// },
+		{
+			field: "created_at",
+			headerName: "Ngày tạo",
+			width: 200,
+			editable: true,
+		},
 	];
 
 	return (
 		<Layout>
 			<>
-				<Box sx={{ height: 400, width: "100%" }}>
-					<DataGrid
-						rows={data}
-						columns={columns}
-						initialState={{
-							pagination: {
-								paginationModel: {
-									pageSize: 5,
-								},
-							},
-						}}
-						pageSizeOptions={[5]}
-						checkboxSelection
-						disableRowSelectionOnClick
-					/>
-				</Box>
+				<Row>
+					<Col>
+						<Button variant="primary" className="mb-3" onClick={handleShow}>
+							Đặt lịch ngay
+						</Button>
+					</Col>
+
+					<Modal show={show} onHide={handleClose}>
+						<Form>
+							<Modal.Header closeButton>
+								<Modal.Title>Đặt lịch mới</Modal.Title>
+							</Modal.Header>
+							<Modal.Body>
+								<Row>
+									<Col>
+										<Form.Group className="mb-3" controlId="formCustomer">
+											<Form.Label>Chọn customer</Form.Label>
+											<Form.Select>
+												<option>Chọn 1 người nhận lịch</option>
+												<option value="1">One</option>
+												<option value="2">Two</option>
+												<option value="3">Three</option>
+											</Form.Select>
+										</Form.Group>
+									</Col>
+								</Row>
+							</Modal.Body>
+							<Modal.Footer>
+								<Button variant="secondary" onClick={handleClose}>
+									Thoát ra
+								</Button>
+								<Button variant="primary" onClick={handleClose}>
+									Đặt Ngay
+								</Button>
+							</Modal.Footer>
+						</Form>
+					</Modal>
+				</Row>
+				<Row>
+					<Col>
+						<Box sx={{ height: 400, width: "100%" }}>
+							<DataGrid
+								rows={data}
+								columns={columns}
+								initialState={{
+									pagination: {
+										paginationModel: {
+											pageSize: 5,
+										},
+									},
+								}}
+								pageSizeOptions={[5]}
+								checkboxSelection
+								disableRowSelectionOnClick
+							/>
+						</Box>
+					</Col>
+				</Row>
 			</>
 		</Layout>
 	);
